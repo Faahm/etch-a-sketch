4ccd58fb76abcfac4346b5858dbbfaa396f3df17
Faahm <fatimahussien2@gmail.com>
2022-11-20 23:22:08 +0800

    Add range input

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const container = document.querySelector('.container');
-for(let i = 0; i < 16; i++) {
-    const grid = document.createElement('div');
-    grid.classList.add('grid');
-    console.log(grid);
-    container.appendChild(grid);
+function makeGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+      let grid = document.createElement("div");
+      container.appendChild(grid).className = "grid";
+    };
+    colorBlack()
+  };
+
+  function colorBlack() {
+    const grids = document.querySelectorAll('.grid');
+    grids.forEach(grid => {
+    grid.addEventListener('mouseover', () => {
+      grid.style.backgroundColor = 'black';
+    });
+  });
+  }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("grid-size");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
 }
+
+  makeGrid(16, 16);
